de79884b7255610627e3901f1d8a99efc812a480
Kadir Celep <113991976+Kadirc16@users.noreply.github.com>
2023-04-01 01:39:09 +0300

    Selenium IDE ile oluşturulup refactor edildi.

---
+++ day6/constants/globalConstants.py
@@ -0,0 +1,13 @@
+#Tüm projemizi ilgilendiren değişkenler-sabitleri burada tanımlıyoruz. daha yönetilebilir bir yapı
+
+URL = "https://www.saucedemo.com/"
+idButton = "user-name"
+passwordButton = "password"
+loginButton = "login-button"
+textRemove = "Remove"
+textCheckout = "Checkout"
+checkout = "checkout"
+textNoPassword = "Epic sadface: Password is required"
+textNoUsername = "Epic sadface: Username is required"
+textLockedUser = "Epic sadface: Sorry, this user has been locked out."
+textNotMatchAnything = "Epic sadface: Username and password do not match any user in this service"
